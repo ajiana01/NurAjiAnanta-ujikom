e0579bc142cb6b0c04e2af1bb7b70d3162d176be
Aji Ananta <ajiananta177013@students.unnes.ac.id>
2024-06-19 09:56:48 +0700

    Update: Player movement

---
+++ Assets/Script/Gameplay/Player.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+
+    [SerializeField] private CharacterController _characterController;
+
+    [SerializeField] private float playerSpeed = 30f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementPlayer();
+    }
+
+    void MovementPlayer()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        _characterController.Move(move * playerSpeed);
+        if (move.x > 0)
+        {
+            _animator.SetBool("Right", true);
+            _animator.SetBool("Left", false);
+            _animator.SetBool("Shoot", false);
+        }
+        if (move.x < 0)
+        {
+            _animator.SetBool("Left", true);
+            _animator.SetBool("Right", false);
+            _animator.SetBool("Shoot", false);
+        }
+        if (move.x ==0)
+        {
+            _animator.SetBool("Left", false);
+            _animator.SetBool("Right", false);
+            _animator.SetBool("Shoot", false);
+            _animator.SetBool("Idle", true);
+        }
+    }
+}
